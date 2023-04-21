e573959c12197b0fbedf6f32d16f15a3d94b7bb9
sreejarao2404 <123528171+sreejarao2404@users.noreply.github.com>
2023-04-21 13:31:02 -0500

    sdv

--- app.js
+++ app.js
@@ -19,6 +19,23 @@ var boardRouter = require('./routes/board');
 var selectorRouter = require('./routes/selector');
 var resourceRouter = require('./routes/resource');
 
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+
+passport.use(new LocalStrategy(
+   function(username, password, done) {
+   Account.findOne({ username: username }, function (err, user) {
+   if (err) { return done(err); }
+   if (!user) {
+   return done(null, false, { message: 'Incorrect username.' });
+   }
+   if (!user.validPassword(password)) {
+   return done(null, false, { message: 'Incorrect password.' });
+   }
+   return done(null, user);
+   });
+   }));
+
 //var flowers = require('./routes/flowers');
 
 
@@ -32,6 +49,15 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+app.use(require('express-session')({
+   secret: 'keyboard cat',
+   resave: false,
+   saveUninitialized: false
+  }));
+  app.use(passport.initialize());
+  app.use(passport.session());
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
@@ -80,6 +106,14 @@ async function recreateDB(){
  }
  let reseed = true;
  if (reseed) {recreateDB();}
+
+ // passport config
+// Use the existing connection
+// The Account model
+var Account =require('./models/account');
+passport.use(new LocalStrategy(Account.authenticate()));
+passport.serializeUser(Account.serializeUser());
+passport.deserializeUser(Account.deserializeUser());
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
